366567f0d54564b0a0981be313feb0831c5f41b8
student <stsan@csu.fullerton.edu>
2019-09-14 23:30:23 -0700

    calculator made

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,20 @@
 // Sales prediction
+#include <iostream>
+
+int main()
+{
+
+  double sale;
+// What was last year sale's total?
+  std::cout << "How much was earned in 2018? $";
+  std::cin >> sale;
+
+// Calculate prediction of Sales
+  double increase = sale * 0.18;
+  long predict = sale + increase;
+
+// Display sale Prediction
+  std::cout << "With sales increasing by 18%, this year's sales will be at: $" << predict << '\n';
+
+  return 0;
+}
